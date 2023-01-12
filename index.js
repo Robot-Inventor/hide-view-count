fa83a7eeb13f03a1579a8c9c08d75bbe854c92cc
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2023-01-12 23:17:18 +0900

    スクリーンネームがanalyticsで終わるユーザーのアイコンやスクリーンネームが非表示になるバグを修正 #15 (#21)

--- index.js
+++ index.js
@@ -1,37 +1,44 @@
 const checked_tweets_class = "checked-by-hide-view-count";
-
-const is_view_count_link = (element) => {
-    return element.getAttribute("href").endsWith("analytics");
+const observer_option = {
+    childList: true,
+    subtree: true
 };
 
-// Hide view count for normal timeline view.
-const hide_view_count = () => {
-    const tweets = document.body.querySelectorAll(`[data-testid="tweet"]:not(.${checked_tweets_class})`);
-    tweets.forEach((tweet) => {
-        tweet.classList.add(checked_tweets_class);
+class HideViewCount {
+    constructor(target) {
+        const observer = new MutationObserver(() => {
+            this.timeline();
+            this.detail_view();
+        });
+        observer.observe(target, observer_option);
+    }
 
-        const link_list = [...tweet.querySelectorAll("a[role='link']")];
-        const impression_icon = link_list.filter((e) => is_view_count_link(e));
-        if (!impression_icon.length) return;
+    // Hide view count for normal timeline view.
+    timeline() {
+        const links = document.body.querySelectorAll(`[data-testid="tweet"] a[aria-label][href$="analytics"]:not(.${checked_tweets_class})`);
+        links.forEach((link) => {
+            link.classList.add(checked_tweets_class);
+            // Measures against incompatibility with "Minimal Theme for Twitter" or other extensions. See [#7](https://github.com/Robot-Inventor/hide-view-count/issues/7)
+            link.href = "";
+            const view_count_outer = link.parentElement;
+            view_count_outer.style.display = "none";
+        });
+    }
 
-        // Measures against incompatibility with "Minimal Theme for Twitter" or other extensions. See [#7](https://github.com/Robot-Inventor/hide-view-count/issues/7)
-        impression_icon[0].href = "";
-        const impression_icon_outer = impression_icon[0].parentElement;
-        impression_icon_outer.style.display = "none";
-    });
-};
+    // Hide view count for detail view (reply browsing mode).
+    detail_view() {
+        const time = document.querySelector(`[data-testid="cellInnerDiv"] [data-testid="tweet"] time:not(.${checked_tweets_class})`);
+        time.classList.add(checked_tweets_class);
 
-// Hide view count for detailed view (reply browsing mode).
-const hide_view_count_in_detailed_view = () => {
-    const time = document.querySelector(`[data-testid="cellInnerDiv"] [data-testid="tweet"] time:not(.${checked_tweets_class})`);
-    time.classList.add(checked_tweets_class);
-    const time_and_view_count_outer = time.parentElement.parentElement;
-    // View Count and middle dot
-    const target = time_and_view_count_outer.querySelectorAll('a[role="link"] ~ *');
-    target.forEach((element) => {
-        element.style.display = "none";
-    });
-};
+        const time_and_view_count_outer = time.parentElement.parentElement;
+
+        // View Count and middle dot
+        const target = time_and_view_count_outer.querySelectorAll('a[role="link"] ~ *');
+        target.forEach((element) => {
+            element.style.display = "none";
+        });
+    }
+}
 
 // Wait for timeline to load.
 const body_observer = new MutationObserver(() => {
@@ -41,31 +48,9 @@ const body_observer = new MutationObserver(() => {
     const layer = document.querySelector("#layers");
 
     if (!(timeline && layer)) return;
-
     body_observer.disconnect();
 
-    // Start timeline observing.
-    const main_observer = new MutationObserver(() => {
-        hide_view_count();
-        hide_view_count_in_detailed_view();
-    });
-    main_observer.observe(timeline, {
-        childList: true,
-        subtree: true
-    });
-
-    // Start to observe elements used for image view mode.
-    const layer_observer = new MutationObserver(() => {
-        hide_view_count();
-        hide_view_count_in_detailed_view();
-    });
-    layer_observer.observe(layer, {
-        childList: true,
-        subtree: true
-    });
+    new HideViewCount(timeline);
+    new HideViewCount(layer);
 });
-
-body_observer.observe(document.body, {
-    childList: true,
-    subtree: true
-});
+body_observer.observe(document.body, observer_option);
