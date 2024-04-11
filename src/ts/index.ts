c6523402debd3578a54f76366d15a536f8583029
roboin <50550561+Robot-Inventor@users.noreply.github.com>
2024-04-11 10:03:10 +0900

    feat: install ESLint (#43)

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,10 +1,14 @@
 import { Timeline, Tweet } from "twi-ext";
 
+// eslint-disable-next-line max-statements
 const onNewTweet = (tweet: Tweet): void => {
     const tweetElement = tweet.element;
     const viewCountWithIcon = tweetElement.querySelector<HTMLAnchorElement>("[role='group'] a[href$='analytics']");
     if (viewCountWithIcon && viewCountWithIcon.parentElement) {
-        // Measures against incompatibility with "Minimal Theme for Twitter" or other extensions. See [#7](https://github.com/Robot-Inventor/hide-view-count/issues/7)
+        /*
+         * Measures against incompatibility with "Minimal Theme for Twitter" or other extensions.
+         * See [#7](https://github.com/Robot-Inventor/hide-view-count/issues/7)
+         */
         viewCountWithIcon.href = "";
         viewCountWithIcon.parentElement.style.display = "none";
     }
